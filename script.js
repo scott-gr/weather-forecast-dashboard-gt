21a521d7039b312c3bc20a9c11898031b3dba6a0
Scott Griffin <mscottgriffin@gmail.com>
2020-06-17 09:17:14 -0400

    adding search form, needs more styling

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 $(document).ready(function () {
-  var searchCity = 'atlanta'
+  var searchCity = 'atlanta'; // $.trim("#search-city").val();
   var queryURL =
-    'https://api.openweathermap.org/data/2.5/weather?q=' + searchCity + '&appid=736a813945a5dede2518eacfa29fd0b4';
-  
-    $.ajax({
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    searchCity +
+    '&appid=736a813945a5dede2518eacfa29fd0b4';
+
+  $.ajax({
     url: queryURL,
     method: 'GET',
   }).then(function (response) {
+
+
+    
     console.log(response);
-
-
-
-
   });
 });
