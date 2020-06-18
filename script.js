597d9f1cbbdb3f76c007435c6e720d019e492eda
Scott Griffin <mscottgriffin@gmail.com>
2020-06-18 00:32:09 -0400

    added 2nd query for 5 day forecast

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 $(document).ready(function () {
 // 
 
-  var searchCity = 'atlanta';
-  var queryURL =
-    'https://api.openweathermap.org/data/2.5/weather?q=' +
-    searchCity +
-    '&appid=736a813945a5dede2518eacfa29fd0b4';
-
+  var searchCity = 'atlanta';// change city to test
+  var apiKey = "1935f5d7d75a269680ddfadd7b264dcb";
+// today forecast query and api key
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      searchCity +
+      "&units=imperial&appid=" +
+      apiKey;
+// 5 day forecast queury and key
+    // var queryfiveURL =
+    //   "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    //   searchCity +
+    //   "&units=imperial&appid=" +
+    //   apiKey;
   $.ajax({
     url: queryURL,
     method: 'GET',
