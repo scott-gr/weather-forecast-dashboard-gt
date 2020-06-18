c98821b601f11977ea89732c75c2ed296674d974
Scott Griffin <mscottgriffin@gmail.com>
2020-06-18 01:25:08 -0400

    testing id display

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
 $(document).ready(function () {
-// 
+  //
 
-  var searchCity = 'atlanta';// change city to test
-  var apiKey = "1935f5d7d75a269680ddfadd7b264dcb";
-// today forecast query and api key
-    var queryURL =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      searchCity +
-      "&units=imperial&appid=" +
-      apiKey;
-      //today's date, to be displayed at top of Today element
+  var searchCity = 'atlanta'; // change city to test
+  var apiKey = '1935f5d7d75a269680ddfadd7b264dcb';
+  // today forecast query and api key
+  var queryURL =
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    searchCity +
+    '&units=imperial&appid=' +
+    apiKey;
+  //today's date, to be displayed at top of Today element
+  function getForecast(searchCity) {
     var dateToday = moment().format('(M/D/YYYY'); //id is #today-date
-  $.ajax({
-    url: queryURL,
-    method: 'GET',
-  }).then(function (response) {
-    // console.log(response);
-    //Weather data points from api - to be assigned to text in 'Today' element
-    var tempToday = response.main.temp;
-    var humToday = response.main.humidity;
-    var windsToday = response.main.wind.speed;
-    var iconToday = response.weather[0].icon;
-    var iconTodayUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-    var latToday = response.coord.lat;
-    var lonToday = response.coord.lon;
-
-
-
-    
-
-  });
+    $.ajax({
+      url: queryURL,
+      method: 'GET',
+    }).then(function (response) {
+      // console.log(response);
+      //Weather data points from api - to be assigned to text in 'Today' element
+      var tempToday = response.main.temp;
+      var humToday = response.main.humidity;
+      var windsToday = response.wind.speed;
+      var iconToday = response.weather[0].icon;
+      // var iconTodayUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+      // // latitude and longitude needed for UV Index
+      // var latToday = response.coord.lat;
+      // var lonToday = response.coord.lon;
+      // linking html ids to variables above
+      $('#today-date').text(response.name + ' ' + dateToday);
+      $('#temp-today').text('Temperature: ' + tempToday + ' F');
+      $('#hum-today').text('Humidity: ' + humToday + '%');
+      $('#winds-today').text('Wind: ' + windsToday + ' mph');
+      // $("#icon-today").attr("src", iconToday);
+    });
+  }
+  getForecast();
 });
 
 // 5 day forecast queury and key
-    // var queryfiveURL =
-    //   "https://api.openweathermap.org/data/2.5/forecast?q=" +
-    //   searchCity +
-    //   "&units=imperial&appid=" +
-    //   apiKey;
+// var queryfiveURL =
+//   "https://api.openweathermap.org/data/2.5/forecast?q=" +
+//   searchCity +
+//   "&units=imperial&appid=" +
+//   apiKey;
